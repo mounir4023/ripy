52f7f64bebb7847497665138fea77a6122526da8
devnir <dev.mounir@protonmail.com>
2020-01-06 14:23:28 +0100

    sorted lq confirmed

--- eval.py
+++ eval.py
@@ -7,6 +7,9 @@ from datetime import datetime
 import pickle
 import sys
 import os
+import random
+import math
+import numpy as np
 
 testdir = "./dataset_files"
 manager = DatasetManager(testdir)
@@ -18,17 +21,61 @@ perts = pickle.load(p_perts)
 
 def get_filtered_docs( query , N , S ):
     
+    query = query.lower()
+    query = re.sub(r'\W+',' ',query)
+    qtokens = nltk.word_tokenize(query)
+    stoptokens = Counter(nltk.corpus.stopwords.words('english'))
+    qtokens = [ t for t in qtokens if t not in stoptokens ]
+    query = ' '.join(qtokens)
+    
+    print(query)
+    
     response_docs = manager.docs_of_vectorial_q_mc(query.lower())
-    print(len(response_docs))
+    
     final_docs = [ d for d in response_docs if d[1] > S ]
-    print(len(final_docs))
+    final_docs = [ d[0] for d in final_docs ]
     
-    return final_docs
-    
-N = 10
-S = 0
-q = queries[0]
+    return final_docs[-N:]
 
-get_filtered_docs(q, N, S)
+def get_metrics( query, num, N, S ):
     
+    response_docs = get_filtered_docs(query.lower() , N, S)
+    lq = len ( response_docs )
+    print(response_docs)
+    print("lq",lq)
+    
+    if num<10:
+        num = '0'+str(num)
+    else:
+        num = str(num)
+    true_docs = perts[num]
+    true_docs = [ d+'.cacm' for d in true_docs ]
+    dq = len ( true_docs )
+    print(true_docs)
+    print("dq",dq)
+    
+    #both_docs = [ d for d in response_docs if d in true_docs ]
+    both_docs = [ ]
+    for d in true_docs:
+        if d in response_docs:
+            print(d)
+            both_docs.append(d)
+    
+    bq = len ( both_docs )  
+    print("bq",bq)
+    
+    acc = bq / lq
+    rec = bq / dq
+    
+    return acc, rec
+        
+
+N = 10
+S = 0.1
+num = math.floor ( random.uniform(1,64.99) )
+num = 8
+q = queries[num-1]
+
+print(num)
+print( get_metrics(q,num,N,S) )
     
